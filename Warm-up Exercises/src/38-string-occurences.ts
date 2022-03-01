5809ea5fecb70f55aa3fe96e64a93f7c6edf3688
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-03-02 01:54:26 +0200

    Commit

--- Warm-up Exercises/src/38-string-occurences.ts
+++ Warm-up Exercises/src/38-string-occurences.ts
@@ -1,6 +1,16 @@
-export {};
+export {}
 
-const count = () => {};
+const count = (str: string, char: string) => {
+  let count = 0
+  let loverCase = str.toLowerCase()
+  let splitString = loverCase.split(" ")
+  for (let i = 0; i < splitString.length; i++) {
+    if (splitString[i] === char) {
+      count++
+    }
+  }
+  return count
+}
 
-console.log(count("The quick brown fox jumps over the lazy dog", "the")); // Expected output: 2
-console.log(count("The quick brown fox jumps over the lazy dog", "fox")); // Expected output: 1
+console.log(count("The quick brown fox jumps over the lazy dog", "the")) // Expected output: 2
+console.log(count("The quick brown fox jumps over the lazy dog", "fox")) // Expected output: 1
