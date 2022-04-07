155876db0d1fc32a10da85da952731cfcbf6d010
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-04-07 04:17:28 +0300

    Three forms completed.

--- homework/4MD/src/scripts.ts
+++ homework/4MD/src/scripts.ts
@@ -1,1 +1,25 @@
-console.log('Ready for coding');
+// // function to set a given theme/color-scheme
+// export function setTheme(themeName: string) {
+//   localStorage.setItem("theme", themeName)
+//   document.documentElement.className = themeName
+// }
+
+// // function to toggle between light and dark theme
+// export function toggleTheme() {
+//   if (localStorage.getItem("theme") === "theme-dark") {
+//     setTheme("theme-light")
+//   } else {
+//     setTheme("theme-dark")
+//   }
+// }
+
+// // Immediately invoked function to set the theme on initial load
+// ;(function () {
+//   if (localStorage.getItem("theme") === "theme-dark") {
+//     setTheme("theme-dark")
+//   } else {
+//     setTheme("theme-light")
+//   }
+// })()
+
+// console.log("Hello World")
--- homework/4MD/webpack.config.js
+++ homework/4MD/webpack.config.js
@@ -1,60 +1,61 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-  entry: ['./src/scripts.ts', './src/styles.scss'],
-  mode: 'development',
+  entry: ["./src/scripts.ts", "./src/styles.scss"],
+  mode: "development",
   devServer: {
     port: 3000,
     historyApiFallback: true,
     client: {
-      logging: 'none',
+      logging: "none",
     },
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
           {
-            loader: 'file-loader',
-            options: { name: '[name].css' },
+            loader: "file-loader",
+            options: { name: "[name].css" },
           },
-          'sass-loader',
+          "sass-loader",
         ],
         exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'src/index.html',
-    favicon: 'favicon.png',
-    chunks: [],
-  }),
-  new CopyPlugin({
-    patterns: [
-      {
-        from: 'src/assets',
-        to: 'assets',
-      },
-    ],
-  }),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "favicon.png",
+      chunks: [],
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/assets",
+          to: "assets",
+        },
+      ],
+    }),
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'scripts.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "scripts.js",
   },
-};
+}
