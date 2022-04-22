7e7753b2cafc4bf73f68f869d66545e19617f8a4
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-04-22 22:13:30 +0300

    Fixes

---
+++ Pieraksti/Day 8-9/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
+import Heading from './components/heading/heading'
+
+const App = () => (
+  <div>
+    <Heading text="Hello 1" />
+    <Heading text="Hello 2" />
+  </div>
+)
+
+export default App
