aba0658cf84ee344d61f7c88eda6417976f488f3
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-04-25 21:39:19 +0300

    Updated with react state

--- Pieraksti/Day 8-9/src/App.tsx
+++ Pieraksti/Day 8-9/src/App.tsx
@@ -1,161 +1,80 @@
-import React from 'react'
-import logo from './logo.svg'
 import './App.scss'
-import Article from './components/Article/Article'
-// import Heading from './components/Heading/Heading'
-// import Button from './components/Button/Button'
-// import Paragraph from './components/Paragraph/Paragraph'
-import Hero from './components/Hero/Hero'
-import Shapes from './components/Shapes/Shapes'
+import { useState } from 'react'
 
-// Jāuztaisa masīvs, kurš sastāv no 10 objektiem, kuros ir:
-// krāsa
-// apaļš/kantains
-// rādīt/nerādīt
-// teksts ja teksts ir rādīt to pa vidu, ja nav tad rādīt "empty" un emty ir slīprakstā
+const App = () => {
+  // States
+  // Hooks
+  const [count, setCount] = useState(0)
+  const [showReset, setShowReset] = useState(false)
 
-const articles = [
-  {
-    title: 'Article from array 1',
-    description: 'Article description from array 1',
-    imgSrc: 'https://picsum.photos/500/200',
-    buttonText: 'This is button from array 1',
-  },
-  {
-    title: 'Article from array 2',
-    description: 'Article description from array 2',
-    imgSrc: 'https://picsum.photos/500/200',
-    buttonText: 'This is button from array 2',
-  },
-]
+  const actions = [
+    {
+      title: 'Add + 1',
+      onClick: () => setCount(count + 1),
+    },
+    {
+      title: 'Subtract - 1',
+      onClick: () => setCount(count - 1),
+    },
+    {
+      title: 'Multiply * 2',
+      onClick: () => setCount(count * 2),
+    },
+    {
+      title: 'Divide / 2',
+      onClick: () => setCount(count / 2),
+    },
+    // {
+    //   title: 'Reset',
+    //   onClick: () => setCount(0),
+    // },
+    // {
+    //   title: 'Show reset',
+    //   onClick: () => setShowReset(!showReset),
+    // },
+  ]
 
-const inputData = {
-  placeholder: 'Full name',
-  type: 'text',
-  label: 'Full name',
-  error: '',
-}
+  const endsWithSeven = () => {
+    const lastNumber = count.toString().slice(-1)
+    return lastNumber === '7'
+  }
 
-const shapesArray = [
-  {
-    color: 'red',
-    rounded: true,
-    show: false,
-    text: '',
-  },
-  {
-    color: 'blue',
-    rounded: false,
-    show: true,
-    text: 'Square',
-  },
-  {
-    color: 'gren',
-    rounded: true,
-    show: false,
-    text: '',
-  },
-  {
-    color: 'yellow',
-    rounded: false,
-    show: true,
-    text: '',
-  },
-  {
-    color: 'orange',
-    rounded: false,
-    show: false,
-    text: '',
-  },
-  {
-    color: 'gold',
-    rounded: false,
-    show: true,
-    text: 'This is golden',
-  },
-  {
-    color: 'purple',
-    rounded: true,
-    show: true,
-    text: '',
-  },
-  {
-    color: 'coral',
-    rounded: false,
-    show: true,
-    text: 'Coral color',
-  },
-  {
-    color: 'pink',
-    rounded: true,
-    show: true,
-    text: '',
-  },
-  {
-    color: 'lightgreen',
-    rounded: false,
-    show: true,
-    text: 'Texting',
-  },
-]
-
-const App = () => (
-  <div className="container container-fluid">
-    <div className="row">
-      <div className="col-xs-12">
-        <div className="box-container">
-          {shapesArray
-            .filter(({ show }) => show)
-            .map(({
-              color, rounded, text,
-            }) => (
-              <Shapes rounded={rounded} text={text} color={color} />
+  return (
+    <div className="container container-fluid">
+      <div className="row">
+        <div className="col-xs-12">
+          <section>
+            <h1 style={endsWithSeven() ? { color: 'gold' } : { color: 'black' }}>{count}</h1>
+            {actions.map((action) => (
+              <button
+                type="button"
+                key={Math.random()}
+                onClick={action.onClick}
+              >
+                {action.title}
+              </button>
             ))}
+            <button
+              type="button"
+              key={Math.random()}
+              onClick={() => setShowReset(!showReset)}
+            >
+              {showReset ? 'Hide reset' : 'Show reset'}
+            </button>
+            {showReset && (
+              <button
+                type="button"
+                key={Math.random()}
+                onClick={() => setCount(0)}
+              >
+                Reset
+              </button>
+            )}
+          </section>
         </div>
       </div>
     </div>
-
-    <div className="row">
-      {articles.map((article) => (
-        <div className="col-xs-4" key={article.title}>
-          <Article
-            title={article.title}
-            description={article.description}
-            imgSrc={article.imgSrc}
-            buttonText={article.buttonText}
-            onClick={() => { console.log('Article clicked') }}
-          />
-        </div>
-      ))}
-    </div>
-    <div className="row">
-      <div className="col-xs-12">
-        <Hero
-          heading="This is hero"
-          paragraph="This is paragraph"
-          buttonText="This is button"
-          onClick={() => (
-            console.log('Hero')
-          )}
-        />
-      </div>
-    </div>
-
-    <div className="row">
-      <div className="col-xs-12">
-        <label>
-          {inputData.label}
-          <br />
-          <input
-            type={inputData.type}
-            placeholder={inputData.placeholder}
-          />
-          <br />
-          {inputData.error && <span>{inputData.error}</span> }
-        </label>
-      </div>
-    </div>
-  </div>
-)
+  )
+}
 
 export default App
