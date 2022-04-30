271e88da7bce62c1397fb2b1b638d39437ced9ad
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-05-01 01:22:22 +0300

    Few fixes

--- homework/12MD/src/App.tsx
+++ homework/12MD/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.scss'
 import MiniCalculator from './components/MiniCalculator/MiniCalculator'
-import Pizza from './components/AddPizza/AddPizza'
-import CubeColor from './components/AddCubeColor/AddCubeColor'
-import SomeText from './components/AddSomeText/AddSomeText'
-import NumberArray from './components/AddNumberArray/AddNumberArray'
-import Cards from './components/AddCards/AddCards'
+import Pizza from './components/Pizza/Pizza'
+import CubeColor from './components/CubeColor/CubeColor'
+import SomeText from './components/SomeText/SomeText'
+import NumberArray from './components/NumberArray/NumberArray'
+import Cards from './components/Cards/Cards'
 
 const App = () => (
   <div className="container container-fluid">
