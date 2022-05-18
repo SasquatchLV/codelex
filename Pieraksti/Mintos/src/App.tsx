9767cd6b28b0b8790667489ed65c32120689c55a
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-05-18 18:03:43 +0300

    Mintos MD

---
+++ Pieraksti/Mintos/src/App.tsx
@@ -0,0 +1,8 @@
+import './App.scss'
+import CurrencySelector from './components/CurrencySelector/CurrencySelector'
+
+const App = () => (
+  <CurrencySelector />
+)
+
+export default App
---
+++ Pieraksti/Mintos/src/Models/CurrencyModel.ts
@@ -0,0 +1,5 @@
+export type CurrencyType = {
+  id: number,
+  currency: string,
+  isActive: boolean,
+}
