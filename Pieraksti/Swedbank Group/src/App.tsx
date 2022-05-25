102b822e933f08749d7aa77a11fcca57c8f9b2fa
Almighty Sasquatch <almightysasquatch@gmail.com>
2022-05-25 20:26:03 +0300

    Few fixes

--- Pieraksti/Swedbank Group/src/App.tsx
+++ Pieraksti/Swedbank Group/src/App.tsx
@@ -25,155 +25,158 @@ const App = () => {
   return (
     <>
       <div className={styles.container}>
-        <fieldset>
-          {activeQuestion === 0 && (
-            <StepOne
-              onSave={() => {
-                setActiveQuestion(activeQuestion + 1)
-              }}
-            />
-          )}
-
-          {activeQuestion === 1 && (
-            <StepTwo
-              onChange={(value) => {
-                setFormData({ ...formData, fullName: value })
-              }}
-              onBack={() => {
-                setError('')
-                setActiveQuestion(activeQuestion - 1)
-              }}
-              onContinue={
-                () => {
-                  setError('')
-                  if (!formData.fullName) {
-                    setError('Error: Please enter your full name')
-                    return
-                  }
+        <div className={styles.wrapper}>
+          <fieldset>
+            {activeQuestion === 0 && (
+              <StepOne
+                onSave={() => {
                   setActiveQuestion(activeQuestion + 1)
-                }
-              }
-              fullName={formData.fullName}
-            />
-          )}
+                }}
+              />
+            )}
 
-          {activeQuestion === 2 && (
-            <StepThree
-              onRadioChange={(value) => {
-                setFormData({ ...formData, gender: value })
-              }}
-              onBack={() => {
-                setError('')
-                setActiveQuestion(activeQuestion - 1)
-              }}
-              onContinue={() => {
-                setError('')
-                if (!formData.gender) {
-                  setError('Error: Please choose your gender')
-                  return
-                }
-                setActiveQuestion(activeQuestion + 1)
-              }}
-              selectedOption={formData.gender}
-            />
-          )}
-
-          {activeQuestion === 3 && (
-            <>
-              <StepFour
-                onInputChange={(value) => {
-                  setFormData({ ...formData, age: value })
+            {activeQuestion === 1 && (
+              <StepTwo
+                onChange={(value) => {
+                  setFormData({ ...formData, fullName: value })
                 }}
                 onBack={() => {
                   setError('')
                   setActiveQuestion(activeQuestion - 1)
                 }}
-                onContinue={() => {
-                  setError('')
-                  if (formData.age === '') {
-                    setError('Error: Please enter your age')
-                    return
-                  }
-                  setActiveQuestion(activeQuestion + 1)
-                }}
-                selectedOption={formData.age}
-              />
-
-            </>
-          )}
-
-          {activeQuestion === 4 && (
-            <>
-              <StepFive
-                onInputChange={
-                  (likedAnimals) => {
-                    setFormData({ ...formData, likedAnimals: [...likedAnimals] })
+                onContinue={
+                  () => {
+                    setError('')
+                    if (!formData.fullName) {
+                      setError('Error: Please enter your full name')
+                      return
+                    }
+                    setActiveQuestion(activeQuestion + 1)
                   }
                 }
-                likedAnimals={formData.likedAnimals}
-                onBack={() => {
-                  setError('')
-                  setActiveQuestion(activeQuestion - 1)
-                }}
-                onContinue={() => {
-                  setError('')
-                  if (formData.likedAnimals === [] || formData.likedAnimals === undefined) {
-                    setError('Error: Please choose your liked animals')
-                    return
-                  }
-                  setActiveQuestion(activeQuestion + 1)
-                }}
-              />
-            </>
-          )}
-
-          {activeQuestion === 5 && (
-            <>
-              <StepSix
-                onInputChange={
-                  (aboutStr) => {
-                    setFormData({ ...formData, aboutMe: aboutStr })
-                  }
-                }
-                aboutMe={formData.aboutMe}
-                onBack={() => {
-                  setError('')
-                  setActiveQuestion(activeQuestion - 1)
-                }}
-                onContinue={() => {
-                  setError('')
-                  if (formData.aboutMe === '') {
-                    setError('Error: Please write something about yourself')
-                    return
-                  }
-                  setActiveQuestion(activeQuestion + 1)
-                }}
-              />
-            </>
-          )}
-
-          {activeQuestion === 6 && (
-            <>
-              <StepSeven
                 fullName={formData.fullName}
-                gender={formData.gender}
-                age={formData.age}
-                likedAnimals={formData.likedAnimals}
-                aboutMe={formData.aboutMe}
+              />
+            )}
+
+            {activeQuestion === 2 && (
+              <StepThree
+                onRadioChange={(value) => {
+                  setFormData({ ...formData, gender: value })
+                }}
                 onBack={() => {
                   setError('')
                   setActiveQuestion(activeQuestion - 1)
                 }}
+                onContinue={() => {
+                  setError('')
+                  if (!formData.gender) {
+                    setError('Error: Please choose your gender')
+                    return
+                  }
+                  setActiveQuestion(activeQuestion + 1)
+                }}
+                selectedOption={formData.gender}
               />
-            </>
-          )}
-        </fieldset>
-        {error && (
-          <div className={styles.error}>
-            <p>{error}</p>
-          </div>
-        )}
+            )}
 
+            {activeQuestion === 3 && (
+              <>
+                <StepFour
+                  onInputChange={(value) => {
+                    setFormData({ ...formData, age: value })
+                  }}
+                  onBack={() => {
+                    setError('')
+                    setActiveQuestion(activeQuestion - 1)
+                  }}
+                  onContinue={() => {
+                    setError('')
+                    if (formData.age === 'Select option') {
+                      setError('Error: Please enter your age')
+                      return
+                    }
+                    setActiveQuestion(activeQuestion + 1)
+                  }}
+                  selectedOption={formData.age}
+                />
+
+              </>
+            )}
+
+            {activeQuestion === 4 && (
+              <>
+                <StepFive
+                  onInputChange={
+                    (likedAnimals) => {
+                      setFormData({ ...formData, likedAnimals: [...likedAnimals] })
+                    }
+                  }
+                  likedAnimals={formData.likedAnimals}
+                  onBack={() => {
+                    setError('')
+                    setActiveQuestion(activeQuestion - 1)
+                  }}
+                  onContinue={() => {
+                    setError('')
+                    if (formData.likedAnimals === [] || formData.likedAnimals === undefined) {
+                      setError('Error: Please choose your liked animals')
+                      return
+                    }
+                    setActiveQuestion(activeQuestion + 1)
+                  }}
+                />
+              </>
+            )}
+
+            {activeQuestion === 5 && (
+              <>
+                <StepSix
+                  onInputChange={
+                    (aboutStr) => {
+                      setFormData({ ...formData, aboutMe: aboutStr })
+                    }
+                  }
+                  aboutMe={formData.aboutMe}
+                  onBack={() => {
+                    setError('')
+                    setActiveQuestion(activeQuestion - 1)
+                  }}
+                  onContinue={() => {
+                    setError('')
+                    if (formData.aboutMe === '') {
+                      setError('Error: Please write something about yourself')
+                      return
+                    }
+                    setActiveQuestion(activeQuestion + 1)
+                  }}
+                />
+              </>
+            )}
+
+            {activeQuestion === 6 && (
+              <>
+                <StepSeven
+                  fullName={formData.fullName}
+                  gender={formData.gender}
+                  age={formData.age}
+                  likedAnimals={formData.likedAnimals}
+                  aboutMe={formData.aboutMe}
+                  onBack={() => {
+                    setError('')
+                    setActiveQuestion(activeQuestion - 1)
+                  }}
+                />
+              </>
+            )}
+          </fieldset>
+
+          {error && (
+            <div className={styles.error}>
+              <p>{error}</p>
+            </div>
+          )}
+
+        </div>
       </div>
 
     </>
